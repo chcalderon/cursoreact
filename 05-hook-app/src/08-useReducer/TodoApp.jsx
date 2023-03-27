7351ed0843aef4d6b712c35596de093dd71edb72
Christian Calderon <chcalderon@gmail.com>
2023-03-27 02:59:14 -0300

    leccion 145

---
+++ 05-hook-app/src/08-useReducer/TodoApp.jsx
@@ -0,0 +1,44 @@
+import { useReducer } from 'react';
+import { TodoAdd } from './components/TodoAdd';
+import { TodoList } from './components/TodoList';
+import { todoReducer } from './todoReducer';
+
+const initialState = [
+  {
+    id: new Date().getTime(),
+    description: 'Recolectar la piedra del alma',
+    done: false,
+  },
+  {
+    id: new Date().getTime()*3,
+    description: 'Recolectar la piedra del tiempo',
+    done: false,
+  }
+]
+
+export const TodoApp = () => {
+  
+  const [todos, dispatch] = useReducer(todoReducer, initialState)
+
+  const handleNewTodo = (todo) => {
+    console.log({todo});
+  }
+  return (
+    <>
+        <h1>TodoApp: 10, <span>pendientes: 2</span></h1>
+        <hr />
+        <div className="row">
+          <div className="col-7">
+            <TodoList todos={todos} />
+
+          </div>
+          <div className="col-5">
+            <h4>Agregar TODO</h4>
+            <hr />
+            <TodoAdd onNewTodo = {handleNewTodo} />
+          </div>
+        </div>
+        
+    </>
+  )
+}
