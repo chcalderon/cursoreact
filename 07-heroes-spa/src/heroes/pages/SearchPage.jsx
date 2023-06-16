b58bcb45d6276a2264c8fb4928462f16fd405c68
Christian Calderon <chcalderon@gmail.com>
2023-06-16 00:45:26 -0400

    Seccion 14 completo depurado

--- 07-heroes-spa/src/heroes/pages/SearchPage.jsx
+++ 07-heroes-spa/src/heroes/pages/SearchPage.jsx
@@ -74,7 +74,7 @@ export const SearchPage = () => {
                 <HeroCard key={hero.id} {...hero} />
               ))
             }
-            {/*  */}
+            
           </div>
 
        </div>
